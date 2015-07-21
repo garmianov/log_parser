297ee3a67105b6b0e40315d825ddb905be06da53
Gancho Armianov <garmianov@gmail.com>
2015-07-21 15:31:56 -0700

    Defined functions for option with and without specific search term

--- log-parser-v2.py
+++ log-parser-v2.py
@@ -11,31 +11,51 @@ import os
 import sys
 import pprint
 
-
-terms = ['watchdog', 'Unhandled fault', 'Failed to authenticate', 'Link is', 'Ping overdue',
-         'ValidateAndUpdateStreams:Writing Configuration', 'Started at', 'CameraDescriptor:', 'Current boot version:','rebootSystem', 'set resolution to', 'Decode error', 'Overdue', 'Video Present', 'Video Lost',
-         'Timeout during', 'Connecting to rtsp:', 'RTSP \[[0-3]\]', 'Fps', 'Bitrate']
-
-# text = pyperclip.paste()
 found=[]
 lines = []
 text = []
 flist = sys.argv[1] # receives the target directory from the CLI argument
 flist1 = os.listdir(flist)
 os.chdir(sys.argv[1]) # Changes the present working directory to the one from the CLI argument
-with open("sresults", "w") as ffound:
-    for fname in flist1:
-        if 'health' not in str(fname):
-            with open(fname, "r", encoding="ISO-8859-1") as file:
-                print('\r', "File name is ", fname)
-                for line in file:
-                    for y in terms:
-                        if y in line:
+
+def searchargv(terms, flist1, found):
+    with open("sresults", "w") as ffound:
+        for fname in flist1:
+            if 'health' not in str(fname):
+                with open(fname, "r", encoding="ISO-8859-1") as file:
+                    print('\r', "File name is ", fname)
+                    for line in file:
+                        if terms in line:
                             ffound.write(line)
                             found.append(line) # add the found lines to the FOUND list
-ffound.close()
-text = '\n'.join(found)
-print(text)
+    ffound.close()
+    text = '\n'.join(found)
+    print(text)
+
+def searchterms(terms, flist1, found):
+    with open("sresults", "w") as ffound:
+        for fname in flist1:
+            if 'health' not in str(fname):
+                with open(fname, "r", encoding="ISO-8859-1") as file:
+                    print('\r', "File name is ", fname)
+                    for line in file:
+                        for y in terms:
+                            if y in line:
+                                ffound.write(line)
+                                found.append(line) # add the found lines to the FOUND list
+    ffound.close()
+    text = '\n'.join(found)
+    print(text)
+    
+if len(sys.argv) > 2:
+    terms = sys.argv[2]
+    searchargv(terms, flist1, found)
+else:
+    terms = ['watchdog', 'Unhandled fault', 'Failed to authenticate', 'Link is', 'Ping overdue','ValidateAndUpdateStreams:Writing Configuration', 'Started at', 'CameraDescriptor:', 'Current boot version:','rebootSystem', 'set resolution to', 'Decode error', 'Overdue', 'Video Present', 'Video Lost','Timeout during', 'Connecting to rtsp:', 'RTSP \[[0-3]\]', 'Fps', 'Bitrate']
+    searchterms(terms, flist1, found)
+# text = pyperclip.paste()
+
+
 
 
 
