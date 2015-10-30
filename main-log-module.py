286a201ff301eb4d17ba753ab9b6d9a1d031936c
Gancho Armianov <garmianov@gmail.com>
2015-10-30 15:56:01 -0700

    New main module

---
+++ main-log-module.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+'''
+This module will call log-parse-v3.py
+Doing that in order to be able to use it with recursive log directories
+'''
+
+import log-parser-v3.py
+
+
+log-parser-v3.py.connect_rtsp(flist1, rtsp_found, datedic, datedic1)
